21b8d480fc8b1113db77ed36c94d255142e2f5a1
maxence.nho <maxence.nho@hetic.net>
2024-02-01 15:45:49 +0100

    init

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_s3_bucket" "mt4-maxence-hetic" {
+  bucket = "mt4-maxence-hetic"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
